72f09a2d0b3f47cd347c9bbf9cf3fa71163ab1b1
João Vitor de Oliveira Rodrigues <jv-rodrigues1999@hotmail.com>
2021-09-17 21:17:45 -0300

    changing switch case to object

--- index.js
+++ index.js
@@ -83,199 +83,212 @@ app.on('messageCreate', async message => {
         selfDeafen: true
     });
 
-    if (cmd && cmd.length) {
-        switch (cmd) {
-            case "ping":
-                message.reply('Pinging the API...').then(msg => {
-                    msg.edit({ content: `> **API PING** \`${app.ws.ping}\`\n\n> **BOT PING:** \`${(Date.now() - msg.createdTimestamp) - (2 * app.ws.ping)}\`` })
-                }).catch(console.error)
-                break;
-            case "help":
-                const embed = new MessageEmbed()
-                    .setColor("BLURPLE")
-                    .setTitle("Embed")
-                    .setDescription(`Hello I am ${app.user.username} and I am a cool bot!\n\n**These are my Commands:**`)
-                    .setThumbnail(app.user.displayAvatarURL())
-                    .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
-                    .addFields([
-                        { name: "**ping**", value: `> *Shows the Ping of me*`, inline: true },
-                        { name: "**help**", value: `> *Gives you help*`, inline: true },
-                        { name: "**play**", value: `> *Start to play the song directly*`, inline: true },
-                        { name: "**search**", value: `> *Start to play the song, after choose it*`, inline: true },
-                        { name: "**queue**", value: `> *Show the song queue*`, inline: true },
-                        { name: "**q**", value: `> *Alias for queue*`, inline: true },
-                        { name: "**pause**", value: `> *Pause the song*`, inline: true },
-                        { name: "**p**", value: `> *Alias for pause*`, inline: true },
-                        { name: "**resume**", value: `> *Resume the song*`, inline: true },
-                        { name: "**r**", value: `> *Alias for resume*`, inline: true },
-                        { name: "**skip**", value: `> *Skip the song*`, inline: true },
-                        { name: "**s**", value: `> *Alias for skip*`, inline: true },
-                        { name: "**disconnet**", value: `> *Disconnect the DJ*`, inline: true },
-                        { name: "**quit**", value: `> *Alias for diconnect*`, inline: true },
-                        { name: "**Shuffle**", value: `> *Shuffle the queue*`, inline: true },
-                        { name: "**mix**", value: `> *Alias for shuffle*`, inline: true },
-                    ])
+    const cases = {
+        "ping": () => {
+            message.reply('Pinging the API...').then(msg => {
+                msg.edit({ content: `> **API PING** \`${app.ws.ping}\`\n\n> **BOT PING:** \`${(Date.now() - msg.createdTimestamp) - (2 * app.ws.ping)}\`` })
+            }).catch(console.error)
+        },
+        "help": () => {
+            const embed = new MessageEmbed()
+                .setColor("BLURPLE")
+                .setTitle("Embed")
+                .setDescription(`Hello I am ${app.user.username} and I am a cool bot!\n\n**These are my Commands:**`)
+                .setThumbnail(app.user.displayAvatarURL())
+                .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
+                .addFields([
+                    { name: "**ping**", value: `> *Shows the Ping of me*`, inline: true },
+                    { name: "**help**", value: `> *Gives you help*`, inline: true },
+                    { name: "**play**", value: `> *Start to play the song directly*`, inline: true },
+                    { name: "**search**", value: `> *Start to play the song, after choose it*`, inline: true },
+                    { name: "**queue**", value: `> *Show the song queue*`, inline: true },
+                    { name: "**q**", value: `> *Alias for queue*`, inline: true },
+                    { name: "**pause**", value: `> *Pause the song*`, inline: true },
+                    { name: "**p**", value: `> *Alias for pause*`, inline: true },
+                    { name: "**resume**", value: `> *Resume the song*`, inline: true },
+                    { name: "**r**", value: `> *Alias for resume*`, inline: true },
+                    { name: "**skip**", value: `> *Skip the song*`, inline: true },
+                    { name: "**s**", value: `> *Alias for skip*`, inline: true },
+                    { name: "**disconnet**", value: `> *Disconnect the DJ*`, inline: true },
+                    { name: "**quit**", value: `> *Alias for diconnect*`, inline: true },
+                    { name: "**Shuffle**", value: `> *Shuffle the queue*`, inline: true },
+                    { name: "**mix**", value: `> *Alias for shuffle*`, inline: true },
+                    { name: "**clear**", value: `> *Clears the queue*`, inline: true },
+                ])
 
-                message.reply({
-                    embeds: [embed]
+            message.reply({
+                embeds: [embed]
+            })
+        },
+        "play": async () => {
+            if (!args[0]) {
+                message.reply(':warning: **!play** *needs a query!*\n\nExample:\n!play mc poze nos anos 80')
+                return
+            }
+            const res = await app.manager.search(
+                message.content.slice(6),
+                message.author
+            );
+
+            // Connect to the voice channel.
+            if (player.state === 'DISCONNECTED')
+                player.connect();
+
+            if (res.loadType === 'PLAYLIST_LOADED') {
+                res.tracks.forEach(track => {
+                    player.queue.add(track);
                 })
-                break
-            case "play":
-                if (!args[0]) {
-                    message.reply(':warning: **!play** *needs a query!*\n\nExample:\n!play mc poze nos anos 80')
-                    return
+
+                message.channel.send(`Enqueuing ${res.tracks.length} tracks from ${res.playlist.name}.`);
+            }
+            else {
+                player.queue.add(res.tracks[0]);
+                message.channel.send(`Enqueuing track ${res.tracks[0].title}.`);
+            }
+
+
+            // Plays the player (plays the first track in the queue).
+            // The if statement is needed else it will play the current track again
+            if (!player.playing && !player.paused && !player.queue.size)
+                player.play();
+
+            // For playlists you'll have to use slightly different if statement
+            if (
+                !player.playing &&
+                !player.paused &&
+                player.queue.totalSize === res.tracks.length
+            )
+                player.play();
+        },
+        "queue": () => {
+            fields = player.queue.map((item, index) => {
+                return { name: `**${index+1}.**`, value: `> *${item.title} (${item.author})*` }
+            })
+
+            const embed = new MessageEmbed()
+                .setColor("BLURPLE")
+                .setTitle("Music queue")
+                .setThumbnail(app.user.displayAvatarURL())
+                .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
+                .addFields([
+                    { name: `**Current playing:**`, value: `> *${player.queue.current.title} (${player.queue.current.author})*` },
+                    ...fields
+                ])
+
+            message.reply({
+                embeds: [embed]
+            })
+        },
+        "q": () => {
+            fields = player.queue.map((item, index) => {
+                return { name: `**${index+1}.**`, value: `> *${item.title} (${item.author})*` }
+            })
+
+            const embed = new MessageEmbed()
+                .setColor("BLURPLE")
+                .setTitle("Music queue")
+                .setThumbnail(app.user.displayAvatarURL())
+                .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
+                .addFields([
+                    { name: `**Current playing:**`, value: `> *${player.queue.current.title} (${player.queue.current.author})*` },
+                    ...fields
+                ])
+
+            message.reply({
+                embeds: [embed]
+            })
+        },
+        "search": async () => {
+            if (!args || !args.length) {
+                message.reply(':warning: *Command **!search** needs a query*\nExample:\n *!search shake it bololo*')
+                return
+            }
+
+            let search = args.join(' ')
+            const res = await app.manager.search(
+                search,
+                message.author,
+            );
+
+            fields = res.tracks.map((item, index) => {
+                if (index > 9) return
+                return { name: `**${index+1}.**`, value: `> *${item.title} (${item.author})*` }
+            }).filter(item => item)
+
+            const embed = new MessageEmbed()
+                .setColor("BLURPLE")
+                .setTitle("Music queue")
+                .setThumbnail(app.user.displayAvatarURL())
+                .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
+                .addFields(fields)
+
+            message.reply({
+                embeds: [embed]
+            })
+
+            const filter = m => m.author.id === message.author.id && message.content?.length
+            const collector = message.channel.createMessageCollector({filter, max: 1})
+
+            collector.once(`end`, (collected, reason) => {
+                if (reason === 'limit') {
+                    let selected = res.tracks[collected.first().content - 1]
+                    player.queue.add(selected);
+                    message.channel.send(`Enqueuing track ${selected.title}.`);
+
+                    if (player.state === 'DISCONNECTED')
+                        player.connect();
+
+                    if (!player.playing && !player.paused && !player.queue.size)
+                        player.play();
                 }
-                const res = await app.manager.search(
-                    message.content.slice(6),
-                    message.author
-                );
-
-                // Connect to the voice channel.
-                if (player.state === 'DISCONNECTED')
-                    player.connect();
-
-                if (res.loadType === 'PLAYLIST_LOADED') {
-                    res.tracks.forEach(track => {
-                        player.queue.add(track);
-                    })
-
-                    message.channel.send(`Enqueuing ${res.tracks.length} tracks from ${res.playlist.name}.`);
-                }
-                else {
-                    player.queue.add(res.tracks[0]);
-                    message.channel.send(`Enqueuing track ${res.tracks[0].title}.`);
-                }
-
-
-                // Plays the player (plays the first track in the queue).
-                // The if statement is needed else it will play the current track again
-                if (!player.playing && !player.paused && !player.queue.size)
-                    player.play();
-
-                // For playlists you'll have to use slightly different if statement
-                if (
-                    !player.playing &&
-                    !player.paused &&
-                    player.queue.totalSize === res.tracks.length
-                )
-                    player.play();
-                break
-            case "q":
-            case "queue":
-                {
-                    fields = player.queue.map((item, index) => {
-                        return { name: `**${index+1}.**`, value: `> *${item.title} (${item.author})*` }
-                    })
-
-                    const embed = new MessageEmbed()
-                        .setColor("BLURPLE")
-                        .setTitle("Music queue")
-                        .setThumbnail(app.user.displayAvatarURL())
-                        .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
-                        .addFields([
-                            { name: `**Current playing:**`, value: `> *${player.queue.current.title} (${player.queue.current.author})*` },
-                            ...fields
-                        ])
-
-                    message.reply({
-                        embeds: [embed]
-                    })
-                }
-                break
-            case "search":
-                {
-                    if (!args || !args.length) {
-                        message.reply(':warning: *Command **!search** needs a query*\nExample:\n *!search shake it bololo*')
-                        return
-                    }
-
-                    let search = args.join(' ')
-                    const res = await app.manager.search(
-                        search,
-                        message.author,
-                    );
-
-                    fields = res.tracks.map((item, index) => {
-                        if (index > 9) return
-                        return { name: `**${index+1}.**`, value: `> *${item.title} (${item.author})*` }
-                    }).filter(item => item)
-
-                    const embed = new MessageEmbed()
-                        .setColor("BLURPLE")
-                        .setTitle("Music queue")
-                        .setThumbnail(app.user.displayAvatarURL())
-                        .setFooter(message.guild.name, message.guild.iconURL({ dynamic: true }))
-                        .addFields(fields)
-
-                    // const embedButton = new MessageButton()
-                    // embedButton.label = 'A'
-                    // embedButton.
-
-                    message.reply({
-                        embeds: [embed]
-                    })
-
-                    const filter = m => m.author.id === message.author.id && message.content?.length
-                    const collector = message.channel.createMessageCollector({filter, max: 1})
-
-                    collector.once(`end`, (collected, reason) => {
-                        if (reason === 'limit') {
-                            let selected = res.tracks[collected.first().content - 1]
-                            player.queue.add(selected);
-                            message.channel.send(`Enqueuing track ${selected.title}.`);
-
-                            if (player.state === 'DISCONNECTED')
-                                player.connect();
-
-                            if (!player.playing && !player.paused && !player.queue.size)
-                                player.play();
-                        }
-                    })
-                }
-                break
-            case "pause":
-            case "p":
-            case "resume":
-            case "r":
-                {
-                    message.reply(`**${player.playing ? 'Paused' : 'Resumed'}!**`)
-                    player.pause(player.playing)
-                }
-                break
-            case "s":
-            case "skip":
-                {
-                    message.reply(`**Skipped!**`)
-                    player.stop()
-                }
-                break
-            case "disconnect":
-            case "quit":
-                {
-                    if (player.state === 'CONNECTED')
-                        player.destroy()
-                }
-                break
-            case "mix":
-            case "shuffle":
-                {
-                    player.queue.shuffle()
-                    message.reply('**Queue shuffled**')
-                    break
-                }
-            case "clear":
-                {
-                    player.queue.clear()
-                    message.reply(`:wastebasket: The queue is now empty`)
-                }
-                break
-        
-            default:
-                {
-                    message.reply(':x: **Unknown command**')
-                }
-                break;
+            })
+        },
+        "pause": () => {
+            message.reply(`**${player.playing ? 'Paused' : 'Resumed'}!**`)
+            player.pause(player.playing)
+        },
+        "p": () => {
+            message.reply(`**${player.playing ? 'Paused' : 'Resumed'}!**`)
+            player.pause(player.playing)
+        },
+        "resume": () => {
+            message.reply(`**${player.playing ? 'Paused' : 'Resumed'}!**`)
+            player.pause(player.playing)
+        },
+        "r": () => {
+            message.reply(`**${player.playing ? 'Paused' : 'Resumed'}!**`)
+            player.pause(player.playing)
+        },
+        "skip": () => {
+            message.reply(`**Skipped!**`)
+            player.stop()
+        },
+        "s": () => {
+            message.reply(`**Skipped!**`)
+            player.stop()
+        },
+        "disconnect": () => {
+            if (player.state === 'CONNECTED')
+                player.destroy()
+        },
+        "quit": () => {},
+        "mix": () => {
+            player.queue.shuffle()
+            message.reply('**Queue shuffled**')
+        },
+        "shuffle": () => {
+            player.queue.shuffle()
+            message.reply('**Queue shuffled**')
+        },
+        "clear": () => {
+            player.queue.clear()
+            message.reply(`:wastebasket: The queue is now empty`)
         }
+    }
+
+    if (cases[cmd]) cases[cmd]()
+    else {
+        message.reply(':x: **Unknown command**')
     }
 });
 
